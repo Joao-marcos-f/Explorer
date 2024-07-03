76e0dc3c111ff115c5a040c1c5df7652aa2beb33
João Marcos Ferreira Dos Santos Lucas <joaowebdeveloper@gmail.com>
2024-07-03 14:01:46 -0300

    simple_challengers

---
+++ Javascript_Theory/simpleChallenges/script.js
@@ -0,0 +1,71 @@
+/*
+//Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello Word!")
+//Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+let number1 = 5;
+let number2 = 7;
+let sum = number1 + number2;
+console.log(sum)
+//Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+let variavel = 42;
+
+if (typeof variavel === 'number') {
+    console.log("É um número");
+} else {
+    console.log("Não é um número");
+}
+//Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+let variavel = "Olá, mundo!";
+
+if (typeof variavel === 'string') {
+    console.log("É uma string");
+} else {
+    console.log("Não é uma string");
+}
+//Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+let variavel = true;
+
+if (typeof variavel === 'boolean') {
+    console.log("É um booleano");
+} else {
+    console.log("Não é um booleano");
+}
+//Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+let a = 10;
+let b = 5;
+let resultado = a - b;
+
+console.log("Resultado da subtração:", resultado);
+//Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+let a = 10;
+let b = 5;
+let resultado = a * b;
+
+console.log("Resultado da multiplicação:", resultado);
+
+//Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+let a = 10;
+let b = 5;
+let resultado = a / b;
+
+console.log("Resultado da divisão:", resultado);
+
+//Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+let variavel = 8;
+
+if (typeof variavel === 'number' && variavel % 2 === 0) {
+    console.log("É um número par");
+} else {
+    console.log("Não é um número par");
+}
+
+// Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let variavel = 7;
+
+if (typeof variavel === 'number' && variavel % 2 !== 0) {
+    console.log("É um número ímpar");
+} else {
+    console.log("Não é um número ímpar");
+}
+*/

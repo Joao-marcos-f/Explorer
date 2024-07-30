7ba64108fd800fc496efc1c01aab6dfa89ccc647
João Marcos Ferreira Dos Santos Lucas <joaowebdeveloper@gmail.com>
2024-07-30 01:01:54 -0300

    rodando spa com node
    mudança na rota, e criação dos package do node para rodar um pequeno servidor

--- project-14/JS/index.js
+++ project-14/JS/index.js
@@ -1,10 +1,9 @@
 import { Router } from "./router.js"
 
 const router = new Router()
-router.add('/','/project-14/pages/home.html')
-router.add('/about','/project-14/pages/about.html')
-router.add('/contato','/project-14/pages/contato.html')
-router.add(404,'/project-14/pages/404.html')
+router.add('/','pages/home.html')
+router.add('/about','pages/about.html')
+router.add('/contato','pages/contato.html')
 
 router.handle()
 
